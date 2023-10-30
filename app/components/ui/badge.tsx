fb5b452c26f537e4665bbb2a2bc79dda19c387a3
Michael Schwobe <mschwobe@gmail.com>
2023-10-30 11:11:37 -0500

    Updated `Badge` CSS display to `inline-block`

--- app/components/ui/badge.tsx
+++ app/components/ui/badge.tsx
@@ -14,7 +14,7 @@ export const Badge = forwardRef<React.ElementRef<"span">, BadgeProps>(
       <span
         {...props}
         className={cn(
-          "rounded bg-slate-200 px-1 py-0.5 text-xs font-medium leading-none text-black dark:bg-slate-700 dark:text-slate-300",
+          "inline-block rounded bg-slate-200 px-1 py-0.5 text-center text-xs font-medium leading-none text-black dark:bg-slate-700 dark:text-slate-300",
           className,
         )}
         ref={forwardedRef}

446e9d4fca0b54c81e3f291a5deb9a89b8ebba39
Michael Schwobe <mschwobe@gmail.com>
2023-12-11 11:57:28 -0600

    Updated `getStatus` with better error responses

--- app/utils/status.server.ts
+++ app/utils/status.server.ts
@@ -1,6 +1,6 @@
 import { promiseAllSettledUnion } from "~/utils/misc";
 
-async function getStatus(input: string, timeout: number) {
+export async function getStatus(input: string, timeout: number) {
   try {
     const { ok, status, statusText } = await fetch(input, {
       method: "HEAD",
@@ -9,16 +9,24 @@ async function getStatus(input: string, timeout: number) {
     return { ok, status, statusText };
   } catch (error) {
     if (error instanceof Error) {
-      if ("type" in error && error.type === "aborted") {
-        return { ok: false, status: 408, statusText: "Aborted" };
+      if (error.name === "TimeoutError") {
+        return { ok: false, status: 408, statusText: "Timeout" };
+      } else if (error.name === "AbortError") {
+        return { ok: false, status: 504, statusText: "Aborted" };
       } else {
-        return { ok: false, status: 500, statusText: error.message };
+        return {
+          ok: false,
+          status: 500,
+          statusText: error.name ?? error.message?.slice?.(0, 8) ?? "Unknown",
+        };
       }
     } else {
-      return { ok: false, status: 500, statusText: "Unknown" };
+      return { ok: false, status: 500, statusText: "Internal" };
     }
   }
 }
+
+export type GetStatusData = Awaited<ReturnType<typeof getStatus>>;
 
 export async function getStatuses<TData extends { url: string }>(
   items: TData[],
